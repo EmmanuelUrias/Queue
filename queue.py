b6bbc41d32e8cb2989aed9284a9daff52f72a64b
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-07 13:42:23 -0700

    first queue

--- queue.py
+++ queue.py
@@ -1,2 +1,10 @@
 # You can use a list in python to make a queue
 walmart_stick_price_queue = []
+
+walmart_stick_price_queue.insert(0, 131.10)
+walmart_stick_price_queue.insert(0, 131.12)
+walmart_stick_price_queue.insert(0, 135)
+
+print(walmart_stick_price_queue)
+print(walmart_stick_price_queue.pop())
+print(walmart_stick_price_queue.pop())
